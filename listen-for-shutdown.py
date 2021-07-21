0c021c74cc5b52dde9c8214a35bba811729f7614
sammlapp <sammlapp@gmail.com>
2021-07-21 12:24:22 -0400

    remove all drives in /media/pi before shutdown

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -9,4 +9,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+# remove any drives from /media/pi/
+subprocess.call('sudo rm -rf /media/pi/*', shell=True)
+# shut down the rpi
+subprocess.call(['sudo','shutdown', '-h', 'now'], shell=False)
